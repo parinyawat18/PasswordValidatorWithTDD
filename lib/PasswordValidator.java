3abb98e3346cf8e6dfe9d4f14280707ddfe33e25
parinyawat18 <parinyawat.p@ku.th>
2025-07-04 18:48:13 +0700

    Add files via upload

--- lib/PasswordValidator.java
+++ lib/PasswordValidator.java
@@ -2,18 +2,44 @@ package lib;
 
 public class PasswordValidator {
 
-    /**
+    /**  
      * คุณจะต้องเขียน Javadoc ที่สมบูรณ์ที่นี่ในอนาคต
      * เพื่ออธิบายกฎการทำงานของเมธอด
-     */
-    // TODO: แก้ไข return type ของเมธอดนี้ให้ถูกต้อง
-    public static void validate(String password) { // Function Type ให้เป็น PasswordStrength 
-        
+     *@param password String ที่ต้องการตรวจสอบ
+    @return password PasswordStrength INVALID; 
+   */
+    public static  PasswordStrength validate(String password) { // Function Type ให้เป็น PasswordStrength      
+        if(password==null|| password.length()<8)
+        return PasswordStrength.INVALID;
+
+        boolean hasLower = false ;
+        boolean hasUpper = false ;
+        //boolean Letter = false ;//
+        boolean Special = false ;
+        boolean Digit = false ;
+        for (char c : password.toCharArray()){
+            if(Character.isLowerCase(c))
+            hasLower = true ;
+            else if(Character.isUpperCase(c))
+            hasUpper = true ;
+            else if(Character.isDigit(c))
+            Digit = true ;
+            else Special = true;
+        }
+        if(hasLower && hasUpper  && Digit && Special ) return PasswordStrength.STRONG;
+        if(hasLower && !hasUpper) return PasswordStrength.WEAK;
+
+        else if (hasLower && hasUpper || Digit) return PasswordStrength.MEDIUM;
+
+    
+
+        return PasswordStrength.INVALID ; 
+
         // ส่วนของ Implementation ที่คุณต้องเขียน
-        // ...
+        // 
         
-        int minLength = 8 // TODO: มีอะไรขาดหายไปที่บรรทัดนี้?
         
-        return null ; // TODO: การคืนค่านี้ถูกต้องหรือไม่?
+        
+
     }
 }
